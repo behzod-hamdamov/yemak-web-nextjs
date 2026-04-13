4dd8631a012d260b36f83e7614ff1f08a085ef01
Behzod Hamdamov <bekzodh332@gmail.com>
2026-04-13 14:01:45 +0500

    fix: add CategoryItem props type

--- components/CategoryItem/CategoryItem.tsx
+++ components/CategoryItem/CategoryItem.tsx
@@ -1,4 +1,13 @@
-export const CategoryItem = ({ item }: any) => {
+interface ICategory {
+  id: number,
+  slug: string,
+  title: string,
+  icon: string,
+  "icon_white": string,
+  "seo_text": string
+}
+
+export const CategoryItem = ({ item }: {item: ICategory}) => {
   return (
     <li className="p-[6px_12px] font-semibold leading-5 text-sm text-[#12282F] pr-0 shrink-0 cursor-pointer">
       <p className="border-[#F0F0F0] border-r pr-4 leading-4">{item.title}</p>
